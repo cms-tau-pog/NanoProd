10809491600b73d0323d9c9ca3ffc0cf5018bc68
Konstantin <konstantin.androsov@gmail.com>
2023-10-27 17:27:16 +0200

    Use single CMSSW installation & singularity

--- env.sh
+++ env.sh
@@ -15,23 +15,22 @@ do_install_cmssw() {
 
   export SCRAM_ARCH=$1
   local CMSSW_VER=$2
-  local os_version=$3
-  if ! [ -f "$this_dir/soft/CentOS$os_version/$CMSSW_VER/.installed" ]; then
-    run_cmd mkdir -p "$this_dir/soft/CentOS$os_version"
-    run_cmd cd "$this_dir/soft/CentOS$os_version"
+  if ! [ -f "$this_dir/soft/$CMSSW_VER/.installed" ]; then
+    run_cmd mkdir -p "$this_dir/soft"
+    run_cmd cd "$this_dir/soft"
     run_cmd source /cvmfs/cms.cern.ch/cmsset_default.sh
     if [ -d $CMSSW_VER ]; then
       echo "Removing incomplete $CMSSW_VER installation..."
       run_cmd rm -rf $CMSSW_VER
     fi
-    echo "Creating $CMSSW_VER area for CentOS$os_version in $PWD ..."
+    echo "Creating $CMSSW_VER area in $PWD ..."
     run_cmd scramv1 project CMSSW $CMSSW_VER
     run_cmd cd $CMSSW_VER/src
     run_cmd eval `scramv1 runtime -sh`
     run_cmd ln -s "$this_dir" NanoProd
     run_cmd scram b -j8
     run_cmd cd "$this_dir"
-    run_cmd touch "$this_dir/soft/CentOS$os_version/$CMSSW_VER/.installed"
+    run_cmd touch "$this_dir/soft/$CMSSW_VER/.installed"
   fi
 }
 
@@ -40,25 +39,20 @@ install_cmssw() {
   local this_dir="$( cd "$( dirname "$this_file" )" && pwd )"
   local scram_arch=$1
   local cmssw_version=$2
-  local os_version=$3
-  local target_os_version=$4
-  if [[ $os_version == $target_os_version ]]; then
+  local node_os=$3
+  local target_os=$4
+  if [[ $node_os == $target_os ]]; then
     local env_cmd=""
     local env_cmd_args=""
   else
-    if [[ $target_os_version < 8 ]] ; then
-      local os_type="cc"
-    else
-      local os_type="el"
-    fi
-    local env_cmd="cmssw-$os_type$target_os_version"
+    local env_cmd="cmssw-$target_os"
     if ! command -v $env_cmd &> /dev/null; then
       echo "Unable to do a cross-platform installation for $cmssw_version SCRAM_ARCH=$scram_arch. $env_cmd is not available."
       return 1
     fi
     local env_cmd_args="--command-to-run"
   fi
-  if ! [ -f "$this_dir/soft/CentOS$target_os_version/$CMSSW_VER/.installed" ]; then
+  if ! [ -f "$this_dir/soft/$CMSSW_VER/.installed" ]; then
     run_cmd $env_cmd $env_cmd_args /usr/bin/env -i HOME=$HOME bash "$this_file" install_cmssw $scram_arch $cmssw_version $target_os_version
   fi
 }
@@ -79,11 +73,20 @@ action() {
   run_cmd mkdir -p "$ANALYSIS_DATA_PATH"
 
   local os_version=$(cat /etc/os-release | grep VERSION_ID | sed -E 's/VERSION_ID="([0-9]+).*"/\1/')
-  local default_cmssw_ver=CMSSW_13_0_7
-  export DEFAULT_CMSSW_BASE="$ANALYSIS_PATH/soft/CentOS$os_version/$default_cmssw_ver"
+  if [[ $os_version < 8 ]] ; then
+    local os_prefix="cc"
+  else
+    local os_prefix="el"
+  fi
+  local node_os=$os_prefix$os_version
 
-  run_cmd install_cmssw slc7_amd64_gcc11 $default_cmssw_ver $os_version 7
-  run_cmd install_cmssw el8_amd64_gcc11 $default_cmssw_ver $os_version 8
+  local default_cmssw_ver=CMSSW_13_0_10
+  local target_os_version=8
+  local target_os_prefix="el"
+  local target_os=$target_os_prefix$target_os_version
+  export DEFAULT_CMSSW_BASE="$ANALYSIS_PATH/soft/$default_cmssw_ver"
+
+  run_cmd install_cmssw el8_amd64_gcc11 $default_cmssw_ver $node_os $target_os
 
   if [ ! -z $ZSH_VERSION ]; then
     autoload bashcompinit
@@ -93,7 +96,16 @@ action() {
   source /afs/cern.ch/user/m/mrieger/public/law_sw/setup.sh
 
   source "$( law completion )" ""
-  alias cmsEnv="env -i HOME=$HOME ANALYSIS_PATH=$ANALYSIS_PATH X509_USER_PROXY=$X509_USER_PROXY DEFAULT_CMSSW_BASE=$DEFAULT_CMSSW_BASE $ANALYSIS_PATH/RunKit/cmsEnv.sh"
+
+  if [[ $node_os == $target_os ]]; then
+    export CMSSW_SINGULARITY=""
+    local env_cmd=""
+  else
+    export CMSSW_SINGULARITY="cmssw-$target_os"
+    local env_cmd="$CMSSW_SINGULARITY --command-to-run"
+  fi
+
+  alias cmsEnv="$env_cmd env -i HOME=$HOME ANALYSIS_PATH=$ANALYSIS_PATH X509_USER_PROXY=$X509_USER_PROXY DEFAULT_CMSSW_BASE=$DEFAULT_CMSSW_BASE KRB5CCNAME=$KRB5CCNAME $ANALYSIS_PATH/RunKit/cmsEnv.sh"
 }
 
 if [ "X$1" = "Xinstall_cmssw" ]; then
