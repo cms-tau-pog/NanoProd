b4671def9b94e3c259b5d110b1b7e09ff61156cd
Dennis Roy <dennis.roy@cern.ch>
2024-06-28 10:46:51 +0200

    Add trigger filter bits for embedded samples

--- NanoProd/python/customize.py
+++ NanoProd/python/customize.py
@@ -166,6 +166,83 @@ def customizeEmbedding(process):
   process.updatedJetsPuppi.jetSource = cms.InputTag("slimmedJetsPuppi", "", "MERGE")
   process.jetPuppiCorrFactorsNano.src = cms.InputTag("slimmedJetsPuppi", "", "MERGE")
 
+  # Embedding filter bits
+  # e-leg:
+  '''
+  process.triggerObjectTable.selections[0].qualityBits = cms.string(
+    "filter(’*CaloIdLTrackIdLIsoVL*TrackIso*Filter’) + " \
+    "2*filter(’hltEle*WPTight*TrackIsoFilter*’) + " \
+    "4*filter(’hltEle*WPLoose*TrackIsoFilter’) + " \
+    "8*filter(’*OverlapFilter*IsoEle*PFTau*’) + " \
+    "16*filter(’hltEle*Ele*CaloIdLTrackIdLIsoVL*Filter’) + " \
+    "32*filter(’hltMu*TrkIsoVVL*Ele*CaloIdLTrackIdLIsoVL*Filter*’)+ " \
+    "64*filter(’hltOverlapFilterIsoEle*PFTau*’) + " \
+    "128*filter(’hltEle*Ele*Ele*CaloIdLTrackIdLDphiLeg*Filter’) + " \
+    "256*max(filter(’hltL3fL1Mu*DoubleEG*Filtered*’),filter(’hltMu*DiEle*CaloIdLTrackIdLElectronleg* Filter’)) + " \
+    "512*max(filter(’hltL3fL1DoubleMu*EG*Filter*’),filter(’hltDiMu*Ele*CaloIdLTrackIdLElectronleg* Filter’)) + " \
+    "1024*min(filter(’hltEle32L1DoubleEGWPTightGsfTrackIsoFilter’),filter(’hltEGL1SingleEGOrFilter’) ) + " \
+    "2048*filter(’hltEle*CaloIdVTGsfTrkIdTGsfDphiFilter’) + " \
+    "4096*path(’HLT_Ele*PFJet*’) + " \
+    "8192*max(filter(’hltEG175HEFilter’),filter(’hltEG200HEFilter’)) + " \
+    "16384*filter(’hltEle27WPTightGsfTrackIsoFilter’) + " \
+    "32768*filter(’hltEle32WPTightGsfTrackIsoFilter’) + " \
+    "65536*filter(’hltEle32L1DoubleEGWPTightGsfTrackIsoFilter’) + " \
+    "131072*filter(’hltEGL1SingleEGOrFilter’) + " \
+    "262144*filter(’hltEle35noerWPTightGsfTrackIsoFilter’) + " \
+    "524288*filter(’hltEle24erWPTightGsfTrackIsoFilterForTau’)"
+  )
+  '''
+  # mu-leg:
+  '''
+  process.triggerObjectTable.selections[2].qualityBits = cms.string(
+    "filter(’*RelTrkIsoVVLFiltered0p4’) + " \
+    "2*filter(’hltL3crIso*Filtered0p07’) + " \
+    "4*filter(’*OverlapFilterIsoMu*PFTau*’) + " \
+    "8*max(filter(’hltL3crIsoL1*SingleMu*Filtered0p07’),filter(’hltL3crIsoL1sMu*Filtered0p07’)) + " \
+    "16*filter(’hltDiMuon*Filtered*’) + " \
+    "32*filter(’hltMu*TrkIsoVVL*Ele*CaloIdLTrackIdLIsoVL*Filter*’) + " \
+    "64*filter(’hltOverlapFilterIsoMu*PFTau*’) + " \
+    "128*filter(’hltL3fL1TripleMu*’) + " \
+    "256*max(filter(’hltL3fL1DoubleMu*EG*Filtered*’),filter(’hltDiMu*Ele*CaloIdLTrackIdLElectronleg* Filter’)) + " \
+    "512*max(filter(’hltL3fL1Mu*DoubleEG*Filtered*’),filter(’hltMu*DiEle*CaloIdLTrackIdLElectronleg* Filter’)) + " \
+    "1024*max(filter(’hltL3fL1sMu*L3Filtered50*’),filter(’hltL3fL1sMu*TkFiltered50*’)) + " \
+    "2048*max(filter(’hltL3fL1sMu*L3Filtered100*’),filter(’hltL3fL1sMu*TkFiltered100*’)) + " \
+    "4096*filter(’hltL3crIsoL1sSingleMu22L1f0L2f10QL3f24QL3trkIsoFiltered0p07’) + " \
+    "8192*filter(’hltL3crIsoL1sMu22Or25L1f0L2f10QL3f27QL3trkIsoFiltered0p07’) + " \
+    "16384*filter(’hltL3crIsoL1sMu18erTau24erIorMu20erTau24erL1f0L2f10QL3f20QL3trkIsoFiltered0p07’) + " \
+    "32768*filter(’hltL3crIsoBigORMu18erTauXXer2p1L1f0L2f10QL3f20QL3trkIsoFiltered0p07’)"
+  )
+  '''
+  # tau-leg:
+  '''
+  process.triggerObjectTable.selections[3].sel = cms.string(
+    "( type(84) || type(-100) ) && (pt > 5) && coll(’*Tau*’) && " \
+    "( filter(’*LooseChargedIso*’) || filter(’*MediumChargedIso*’) || " \
+    "filter(’*TightChargedIso*’) || filter(’*TightOOSCPhotons*’) || " \
+    "filter(’hltL2TauIsoFilter’) || filter(’*OverlapFilterIsoMu*’) || " \
+    "filter(’*OverlapFilterIsoEle*’) || filter(’*L1HLTMatched*’) || " \
+    "filter(’*Dz02*’) || filter(’*DoublePFTau*’) || " \
+    "filter(’*SinglePFTau*’) || filter(’hlt*SelectedPFTau’) || " \
+    "filter(’*DisplPFTau*’) || filter(’*Tau*’) )"
+  )
+  process.triggerObjectTable.selections[3].qualityBits = cms.string(
+    "filter(’*LooseChargedIso*’) + " \
+    "2*filter(’*MediumChargedIso*’) + " \
+    "4*filter(’*TightChargedIso*’) + " \
+    "8*filter(’*TightOOSCPhotons*’) + " \
+    "16*filter(’*Hps*’) + " \
+    "32*filter(’hltSelectedPFTau*MediumChargedIsolationL1HLTMatched*’) + " \
+    "64*filter(’hltDoublePFTau*TrackPt1*ChargedIsolation*Dz02Reg’) + " \
+    "128*filter(’hltOverlapFilterIsoEle*PFTau*’) + " \
+    "256*filter(’hltOverlapFilterIsoMu*PFTau*’) + " \
+    "512*filter(’hltDoublePFTau*TrackPt1*ChargedIsolation*’) + " \
+    "1024*filter(’hltL1sBigORLooseIsoEGXXerIsoTauYYerdRMin0p3’) + " \
+    "2048*filter(’hltL1sMu18erTau24erIorMu20erTau24er’) + " \
+    "4096*filter(’hltL1sBigORMu18erTauXXer2p1’) + " \
+    "8192*filter(’hltDoubleL2IsoTau26eta2p2’)"
+  )
+  '''
+
   # Add HF jet shape info (https://github.com/cms-sw/cmssw/blob/CMSSW_10_6_30/PhysicsTools/NanoAOD/python/jets_cff.py#L741-L759)
   from RecoJets.JetProducers.hfJetShowerShape_cfi import hfJetShowerShape
   process.hfJetShowerShapeforNanoAOD = hfJetShowerShape.clone(jets="updatedJets",vertices="offlineSlimmedPrimaryVertices")
