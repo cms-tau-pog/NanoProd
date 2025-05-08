964ffe1558a67860f5ab97900f8f7f7e776d8b31
rkansal47 <raghavk1997@yahoo.co.in>
2025-05-08 11:12:22 -0500

    merge rkansal branch

--- env.sh
+++ env.sh
@@ -55,6 +55,8 @@ do_install_cmssw() {
       run_cmd wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/refs/heads/BoostedDeepTau_v2/BoostedDeepTauId/boosteddeepTau_RunIIv2p0_{core,inner,outer}.pb -P RecoTauTag/TrainingFiles/data/BoostedDeepTauId/
       run_cmd git cms-merge-topic -u cms-tau-pog:CMSSW_14_0_X_tau-pog_DeepTauNoDA
       run_cmd wget https://github.com/cms-tau-pog/RecoTauTag-TrainingFiles/raw/refs/heads/deepTau_v2p5_noDomainAdaptation/DeepTauId/deepTau_2018v2p5_noDomainAdaptation_{core,inner,outer}.pb -P RecoTauTag/TrainingFiles/data/DeepTauId/
+      run_cmd git cms-merge-topic -u rkansal47:CMSSW_14_0_X_HLepRare_skim_2025_v1
+      # git clone https://github.com/cms-data/RecoBTag-Combined $CMSSW_BASE/src/RecoBTag/Combined/data
     fi
 
     run_cmd mkdir NanoProd
