dc3b255917d636f9849e60fce007cfad1e647984
Konstantin <konstantin.androsov@gmail.com>
2025-07-04 10:21:43 +0200

    Updated single top naming

--- NanoProd/test/testConsistency.sh
+++ NanoProd/test/testConsistency.sh
@@ -9,10 +9,6 @@ cd $ANALYSIS_PATH
 for era in Run2_2016 Run2_2016_HIPM Run2_2017 Run2_2018 Run3_2022 Run3_2022EE Run3_2023 Run3_2023BPix; do
   echo "Checking $era consistency..."
   python RunKit/checkTasksConsistency.py --era $era "$@" --dataset-naming-rules NanoProd/crab/dataset_naming_rules.yaml NanoProd/crab/$era/*.yaml
-  if [ $? -ne 0 ]; then
-    echo "Consistency check failed for $era."
-    exit 1
-  fi
 done
 
 
